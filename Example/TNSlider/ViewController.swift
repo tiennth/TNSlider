69311a89e99733499d066759fcc3d5022e1bc91a
Tien Nguyen <thanhtien2302@gmail.com>
2017-04-10 23:42:30 +0700

    Update example

--- Example/TNSlider/ViewController.swift
+++ Example/TNSlider/ViewController.swift
@@ -11,15 +11,34 @@ import TNSlider
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: TNSlider!
+    @IBOutlet weak var stepTextField: UITextField!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderValueChanged(_ sender: TNSlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func minusButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func plusButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func setStepButtonClicked(_ sender: UIButton) {
+        slider.step = Float(stepTextField.text ?? "0") ?? 0
+        print("Step - real value: \(slider.step)")
+    }
 }
 
